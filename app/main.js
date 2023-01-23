f8c5cc81a4aba64d7f5ac6aea438b5f76f1ce6ce
Anurag Choudhury <anuragch198@gmail.com>
2023-01-23 11:40:21 +0530

    second stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+    connection.on("data", (data) => {
+
+        const commands = data.toString().trim();
+        const parts = commands.split(" ");
+        const command = parts[0];
+        if (command.toLowerCase() === "ping") {
+            const response = '+PONG\r\n';
+        } else {
+            const response = '-ERR unknown command\r\n';
+        }
+        connection.write(response);
+
+    });
 });
 
 server.listen(6379, "127.0.0.1");
